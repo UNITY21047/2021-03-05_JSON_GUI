21adc6b592d0084180d572511ff0e59c06e73001
Gary Smith <unity21047@gmail.com>
2021-03-08 15:44:18 -0600

    Major Update:
    Added scaffolding for the application.

--- Program.cs
+++ Program.cs
@@ -7,7 +7,26 @@ namespace _2021_03_05_JSON_GUI
     {
         static void Main(string[] args)
         {
-            
+            /*
+            Section 1:
+            Open environment directory.
+            Get level directories.
+            Open level directories.
+            Get names of all level tile maps.
+            Store names in maps.json example format.
+            Write to maps.json file
+
+            Section 2:
+            Make maps directory.
+            In the maps directory create specified amounts of level directories.
+
+            Section 2 Example:
+            json_project -f 100
+            */
+
+            if(!Directory.Exists(@"./maps")) Directory.CreateDirectory(@".maps");
+
+            if(Int32.Parse()){ for (int i = 0; i < Int32.Parse(args[0]); i++){}
         }
     }
 }
