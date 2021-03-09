527ee8e876e9f443e6da58bb5e771efbd271d2e1
Gary Smith <unity21047@gmail.com>
2021-03-09 10:40:20 -0600

    Major  Update:
    Added the maps folder creation logic + started maps folder json logging.

--- Program.cs
+++ Program.cs
@@ -9,35 +9,60 @@ namespace _2021_03_05_JSON_GUI
         {
             /*
             Section 1:
-            Open environment directory.
-            Get level directories.
-            Open level directories.
-            Get names of all level tile maps.
-            Store names in maps.json example format.
-            Write to maps.json file
+            - Open environment directory.
+            - Get level directories.
+            - Open level directories.
+            - Get names of all level tile maps.
+            - Store names in maps.json example format.
+            - Write to maps.json file.
+            
+            Section 1 Example:
+            json_maps -l
 
             Section 2:
-            Make maps directory.
-            In the maps directory create specified amounts of level directories.
+            - Make maps directory.
+            - In the maps directory create specified amounts of level directories.
 
             Section 2 Example:
-            json_project -f 100
+            json_maps -f 100
+
+            Section 3:
+            - Print out help details.
+
+            Section 3 Example:
+            json_maps -h or json_maps --help
             */
 
-            if(!Directory.Exists(@"./maps")) Directory.CreateDirectory(@".maps");
-
-            try
+            if(args[0].Equals("-l"))
             {
-                Int32.Parse(args[1]);
-
-                for (int i = 0; i < Int32.Parse(args[1]); i++)
+                if(!Directory.Exists(@".\maps"))
                 {
-
+                    Console.WriteLine("maps directory does not exist!");
+                    Console.WriteLine("Use, \"json_maps -f <amount>\"");
                 }
+                else 
+                {
+                    DirectoryInfo current_directory = new DirectoryInfo(Environment.CurrentDirectory + @"\maps");
+                    Console.WriteLine(current_directory.FullName);
+                }
+                
             }
-            catch(Exception e)
+            else if(args[0].Equals("-f"))
             {
-                Console.WriteLine(e);
+                if(!Directory.Exists(@".\maps")) Directory.CreateDirectory(@".\maps");
+
+                try
+                {
+                    Int32.Parse(args[1]);
+                    for (int i = 0; i <= Int32.Parse(args[1]); i++)
+                    {
+                        Directory.CreateDirectory(@".\maps\level_" + i);
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e);
+                }
             }
         }
     }
